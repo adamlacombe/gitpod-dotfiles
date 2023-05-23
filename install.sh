95a809e9a565ecff2efb709ea17aaa4ea36e08a9
Adam LaCombe <adamlacombe@pm.me>
2023-05-23 20:30:20 +0000

    add bitbucket ssh key

--- install.sh
+++ install.sh
@@ -5,6 +5,30 @@ cat >.wakatime.cfg <<EOL
 api_key = $WAKATIME_API_KEY
 EOL
 
+if [ -n "${SSH_PRIVATE_KEY_PERSONAL_BITBUCKET:-}" ]; then
+  echo "SSH_PRIVATE_KEY_PERSONAL_BITBUCKET envvar found"
+
+  echo "${SSH_PRIVATE_KEY_PERSONAL_BITBUCKET}" | base64 -d > "${HOME}/.ssh/bitbucket"
+  chmod 600 "${HOME}/.ssh/bitbucket"
+fi
+
+if [ -n "${SSH_PUBLIC_KEY_PERSONAL_BITBUCKET:-}" ]; then
+  echo "SSH_PUBLIC_KEY_PERSONAL_BITBUCKET envvar found"
+
+  echo "${SSH_PUBLIC_KEY_PERSONAL_BITBUCKET}" | base64 -d > "${HOME}/.ssh/bitbucket.pub"
+  chmod 600 "${HOME}/.ssh/bitbucket.pub"
+fi
+
+eval $(ssh-agent)
+
+ssh-add "${HOME}/.ssh/bitbucket"
+
+cat >.ssh/config <<EOL
+Host bitbucket.org
+  AddKeysToAgent yes
+  IdentityFile ~/.ssh/bitbucket
+EOL
+
 mkdir -p  /workspace/.vscode-remote/data/User/globalStorage/cweijan.vscode-mysql-client2
 cat >/workspace/.vscode-remote/data/User/globalStorage/cweijan.vscode-mysql-client2/config.json <<EOL
 {
